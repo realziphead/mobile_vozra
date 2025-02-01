bba3f42f55e1bdd70cb9d78542620214409c04e0
unknown <ftopsx@gmail.com>
2025-02-01 15:43:33 +0300

    Add new name again 2

--- vozra/apps.py
+++ vozra/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class VozraConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vozra reborn again'
+    name = 'vozra reborn again and again'
